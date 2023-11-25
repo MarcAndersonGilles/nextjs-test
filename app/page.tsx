2833eb0e548ab4390de4176575e4a138748de18b
marc <hornil6748@gmail.com>
2023-11-25 16:37:11 -0500

    some pratice freecodecamp

--- app/page.tsx
+++ app/page.tsx
@@ -17,12 +17,39 @@ const recordCollection = {
     albumTitle: 'ABBA Gold'
   }
 };
+recordCollection
+
+
 
 export default function Home() {
 
-console.log(recordCollection)
+  console.log(recordCollection[2548].artist)
 
-  
+
+
+
+
+  function updateRecords(records, id, prop, value) {
+    if(value === ''){
+    delete records[id][prop]
+  }
+  else if(prop !== 'tracks' && value !== ''){
+      records[id][prop] = value 
+     console.log(value)
+  }
+  else if(prop === 'tracks' && value !== ''){
+    if(records[id].hasOwnProperty('tracks')){
+      records[id][prop].push(value)
+    }
+     else if(records[id].hasOwnProperty('tracks') === false){
+     records[id][prop] = [value]
+    }
+  }
+    return records;
+  }
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+console.log(recordCollection)
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-400">
