bb5032ba7959c117af50e55a43a951cc0dd1cc48
marc <hornil6748@gmail.com>
2023-11-26 20:13:47 -0500

    beginning of the chart

--- app/page.tsx
+++ app/page.tsx
@@ -1,59 +1,18 @@
-const recordCollection = {
-  2548: {
-    albumTitle: 'Slippery When Wet',
-    artist: 'Bon Jovi',
-    tracks: ['Let It Rock', 'You Give Love a Bad Name']
-  },
-  2468: {
-    albumTitle: '1999',
-    artist: 'Prince',
-    tracks: ['1999', 'Little Red Corvette']
-  },
-  1245: {
-    artist: 'Robert Palmer',
-    tracks: []
-  },
-  5439: {
-    albumTitle: 'ABBA Gold'
-  }
-};
-recordCollection
+"use client"
+import React from 'react'
+import ArrayForRatio from '@/components/ArrayForRatio'
+
 
 
 
 export default function Home() {
 
-  console.log(recordCollection[2548].artist)
-
-
-
-
-
-  function updateRecords(records, id, prop, value) {
-    if(value === ''){
-    delete records[id][prop]
-  }
-  else if(prop !== 'tracks' && value !== ''){
-      records[id][prop] = value 
-     console.log(value)
-  }
-  else if(prop === 'tracks' && value !== ''){
-    if(records[id].hasOwnProperty('tracks')){
-      records[id][prop].push(value)
-    }
-     else if(records[id].hasOwnProperty('tracks') === false){
-     records[id][prop] = [value]
-    }
-  }
-    return records;
-  }
-
-updateRecords(recordCollection, 5439, 'artist', 'ABBA');
-console.log(recordCollection)
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-400">
-      <div></div>
+    <main className="flex  min-h-screen flex-col  p-24 bg-gray-400">
+     
+        <ArrayForRatio />
+      
     </main>
   )
 }
---
+++ components/ArrayForRatio.tsx
@@ -0,0 +1,184 @@
+"use client"
+import React, { Fragment, useEffect, useRef, useState } from 'react'
+
+const arrayMonths = [
+    { currentMonth: 'Jan', value: 30 },
+    { currentMonth: 'Feb', value: 330 },
+    { currentMonth: 'Mar', value: 20 },
+    { currentMonth: 'Apr', value: 200 },
+    { currentMonth: 'May', value: 80 },
+    { currentMonth: 'Jun', value: 70 },
+    { currentMonth: 'Jul', value: 60 },
+    { currentMonth: 'Aug', value: 50 },
+    { currentMonth: 'Sep', value: 30 },
+    { currentMonth: 'Oct', value: 30 },
+    { currentMonth: 'Nov', value: 50 },
+    { currentMonth: 'Dec', value: 80 },
+
+]
+
+
+function ArrayForRatio() {
+
+    const [position, setPosition] = useState(0); // Position initiale
+    // const [isMouseOnNav, setisMouseOnNav] = useState(false);
+    // const [disableScroll, setDisableScroll] = useState(false);
+    // const [activeBtn, setActiveBtn] = useState('month');
+
+    //monthly tickets
+    const nonZeroMonths = arrayMonths.filter(item => item.value !== 0);
+    const totalValuesMonths: number = nonZeroMonths.reduce((acc, item) => acc + item.value, 0);
+    const monthsMoy = Math.floor(totalValuesMonths / nonZeroMonths.length);
+    const maxMonthValue = Math.max(...nonZeroMonths.map(item => item.value));
+
+    const chartContainerRef = useRef< HTMLDivElement | null>(null)
+
+     // Maximal hieght for bars graph
+     const containerHeight = 140;
+
+    const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+        const initialX = e.clientX;
+        const containerWidth = (e.target as Element).parentElement?.offsetWidth;
+    
+        // Use the ref to access the chart container
+        const chartContainer = chartContainerRef.current;
+    
+        const handleMouseMove = (event: MouseEvent) => {
+            const movementX = event.clientX - initialX;
+            let newPosition = position + movementX;
+    
+            if (containerWidth !== undefined) {
+                const maxPosition = containerWidth - (e.target as HTMLDivElement).offsetWidth;
+                newPosition = Math.max(0, Math.min(newPosition, maxPosition));
+            }
+    
+            setPosition(newPosition);
+    
+            if (chartContainer) {
+                if (containerWidth !== undefined) {
+                    const maxPosition = containerWidth - (e.target as HTMLDivElement).offsetWidth;
+                    const scrollRatio = newPosition / maxPosition;
+                    chartContainer.scrollLeft =
+                        scrollRatio * (chartContainer.scrollWidth - chartContainer.clientWidth);
+                }
+            }
+        };
+    
+        const handleMouseUp = () => {
+            document.removeEventListener('mousemove', handleMouseMove);
+            document.removeEventListener('mouseup', handleMouseUp);
+        };
+    
+        document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mouseup', handleMouseUp);
+    };
+
+  return (
+
+    <div>
+    <div className='flex  flex-col max-w-[370px] bg-white p-4 rounded-lg gap-4 border-2  '>
+
+        <div className='flex justify-between items-center'>
+            <h2 className='text-figma-neutre-200 font-bold text-xl'>{"dsdfs"}</h2>
+            {/* <IconChevronRight className='w-6 h-6' /> */}
+        </div>
+
+
+        <div className='flex  justify-between w-full flex-wrap gap-4'>
+            <div className='flex items-center gap-3 relative'>
+                <div className='flex relative'>
+
+                   
+
+                   
+
+                </div>
+               
+
+            </div>
+          
+
+        </div>
+
+
+        <div className='flex gap-2 mb-5'>
+            {/* {buttons.map((button, index) => (
+                <button
+                    key={index}
+                    type='button'
+                    className={`flex w-1/3 px-2 py-1.5 text-sm font-medium border-2 rounded-md text-center justify-center ${activeBtn === button.type
+                        ? 'border-primary text-primaryBlack bg-[#EFFCFA]'
+                        : 'border-[#F4F4F5] text-[#52525B] bg-[#F4F4F5] bg-opacity-50'
+                        }`}
+                    onClick={() => handleActifBtn(button.type)}
+                >
+                    {button.label}
+                </button>
+            ))} */}
+        </div>
+
+
+        {/*Graphic chart */}
+        <div className='flex  w-full min-h-[180px]'>
+            <div ref={chartContainerRef} className='chart-container  flex  items-end text-black gap-1 overflow-x-scroll relative  w-full '
+            // onMouseDown={(e) => navMouseDownDrag(e)}
+            >
+
+
+                <div className=' absolute top-10 text-xs font-normal text-[#71717A] z-10  ' style={{ width: '100%', borderBottom: '2px dashed #E4E4E7', margin: '10px 0' }}>
+                    Moy.
+                    {monthsMoy
+                       }</div>
+
+                
+                    <>
+                        {arrayMonths.map((item, index) => (
+                            <Fragment key={index}>
+                                <div className='flex flex-col gap-1 group items-center   justify-center'>
+                                    <div className={`bg-[#F4F4F5] w-7    h-0    ${item.value > 0 ? "p-2.5 w-7 transition-all " : "p-0"} relative`}
+                                        style={
+                                            {
+
+                                                height: `${!isNaN(maxMonthValue)
+                                                    ? `${item.value !== 0 ? (item.value / maxMonthValue) * containerHeight : 0}px`
+                                                    : '0px'
+                                                    }`,
+
+                                            }
+                                        }></div>
+                                    <div className='text-[#71717A] text-xs font-normal text-center'>{item.currentMonth }</div>
+                                    <div className={`absolute     bg-opacity-50    opacity-0 ${item.value > 0 ? "group-hover:opacity-100" : ""}  mb-4  text-gray-500`}>{item.value}</div>
+                                </div>
+                            </Fragment>
+                        )
+                        )}
+                    </>
+               
+               
+              
+
+            </div>
+        </div>
+
+        {/* Scrollbar */}
+        <div className='bg-[#F4F4F5]  w-[80%] rounded-lg h-1 relative '>
+            <div className='w-full'>
+                <div className='w-10 h-1 bg-[#D4D4D8] absolute '
+                    style={{
+                        left: `${position}px`,
+                        transition: 'none', // Désactive la transition pour un déplacement fluide
+                        cursor: 'grab',
+                    }}
+                    onMouseDown={handleMouseDown}
+                ></div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+        
+  )
+}
+
+export default ArrayForRatio
