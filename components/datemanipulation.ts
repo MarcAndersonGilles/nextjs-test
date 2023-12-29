4209e8560eb3ede7c13b7a034c4c3495d1ad53ed
marc <hornil6748@gmail.com>
2023-12-28 21:29:49 -0500

    feature/ ajout currentMonth, nextMoth array

--- components/datemanipulation.ts
+++ components/datemanipulation.ts
@@ -1,5 +1,7 @@
 
+const previousMonthArray = []
 const currentMonthArray = []
+const nextMonthArray = []
 
 const date = new Date()
 
@@ -10,22 +12,41 @@ const getDaysLastMonth = new Date(date.getFullYear(), date.getMonth(), 0)  // 0 
 
 const getFirstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1)
 
-console.log(getFirstDayOfMonth)
-console.log(getDaysOfMonth)
-console.log(getFirstDayOfMonth.getDay()) // console
-console.log(`numOfDaysLastMonth` + getDaysLastMonth.getDate())
-console.log(getFirstDayOfMonth.getDay())
-console.log('num : ' + `${getDaysLastMonth.getDate()} -  ${getFirstDayOfMonth.getDay()}`)
+// console.log(getFirstDayOfMonth)
+// console.log(getDaysOfMonth)
+// console.log(getFirstDayOfMonth.getDay()) // console
+// console.log(`numOfDaysLastMonth` + getDaysLastMonth.getDate())
+// console.log(getFirstDayOfMonth.getDay())
+// console.log('num : ' + `${getDaysLastMonth.getDate()} -  ${getFirstDayOfMonth.getDay()}`)
 
 
-for (let i = getDaysLastMonth.getDate() - (getFirstDayOfMonth.getDay()); i < getDaysLastMonth.getDate(); i++) {
-    currentMonthArray.push(i + 1)
-
+for (let i = getDaysLastMonth.getDate() - (getFirstDayOfMonth.getDay()); i < getDaysLastMonth.getDate(); i++) 
+{
+    previousMonthArray.push(i + 1)
 }
 
+for (let i = 0; i < getDaysOfMonth.getDate(); i++){
+    currentMonthArray.push(i + 1)
+}
+
+const remainingDays = 42 - (previousMonthArray.length + currentMonthArray.length)
 
 
-console.log(currentMonthArray)
+for (let i = 0 ; i < remainingDays; i++){
+    nextMonthArray.push(i + 1)
+}
+
+// console.log(previousMonthArray.length + currentMonthArray.length)
+// console.log(currentMonthArray)
+
+const calendarArray = [...previousMonthArray, ...currentMonthArray, ...nextMonthArray]
+console.log(calendarArray)
+
+
+
+
+
+
 
 
 
