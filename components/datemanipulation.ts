88dfe6c18abb133ec3fb5254112bed355ef8dd0a
marc <hornil6748@gmail.com>
2023-12-28 21:19:08 -0500

    fix previous month not working correctly

--- components/datemanipulation.ts
+++ components/datemanipulation.ts
@@ -4,20 +4,22 @@ const currentMonthArray = []
 const date = new Date()
 
 
-const getDaysOfMonth = new Date(date.getFullYear(), date.getMonth() + 1 ,0)  // 0 is the last day of the month
+const getDaysOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0)  // 0 is the last day of the month
 const getDaysLastMonth = new Date(date.getFullYear(), date.getMonth(), 0)  // 0 is the last day of the month
-const getDaysNextMonth = new Date(date.getFullYear(), date.getMonth() + 2, 0)  // 0 is the last day of the month
+// const getDaysNextMonth = new Date(date.getFullYear(), date.getMonth() + 2, 0)  // 0 is the last day of the month
 
-const getFirstDayOfMonth = new Date(date.getFullYear(), date.getMonth() + 1  , 1)  
+const getFirstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1)
 
 console.log(getFirstDayOfMonth)
+console.log(getDaysOfMonth)
 console.log(getFirstDayOfMonth.getDay()) // console
-console.log(getDaysLastMonth.getDate())
+console.log(`numOfDaysLastMonth` + getDaysLastMonth.getDate())
 console.log(getFirstDayOfMonth.getDay())
+console.log('num : ' + `${getDaysLastMonth.getDate()} -  ${getFirstDayOfMonth.getDay()}`)
 
 
-    for (let i = getDaysLastMonth.getDate() - getFirstDayOfMonth.getDay()  ; i < getDaysLastMonth.getDate(); i++){
-        currentMonthArray.push(i + 1)
+for (let i = getDaysLastMonth.getDate() - (getFirstDayOfMonth.getDay()); i < getDaysLastMonth.getDate(); i++) {
+    currentMonthArray.push(i + 1)
 
 }
 
@@ -28,50 +30,11 @@ console.log(currentMonthArray)
 
 
 
-// function genererArray(nombre:number) {
-//     const resultat = [];
-//     for (let i = nombre - 4; i <= nombre; i++) {
-//       if (i >= 1) {
-//         resultat.push(i);
-//       }
-//     }
-//     return resultat;
-//   }
-  
-//   const nombre = 31;
-//   const monArray = genererArray(nombre);
-//   console.log(monArray);
 
 
-// const currentArrayMonth =[
-     
-//     {
-//         Day: 'Dim', values: {}
-//     },
 
-//     {
-//         Day: 'Lun', values: {}
-//     },
 
-//     {
-//         Day: 'Mar', values: {}
-//     },
 
-//     {
-//         Day: 'Mer', values: {}
-//     },
 
-//     {
-//         Day: 'Jeu', values: {}
-//     },
-
-//     {
-//         Day: 'Ven', values: {}
-//     },
-
-//     {
-//         Day: 'Sam', values: {}
-//     }
-// ]
 
 
